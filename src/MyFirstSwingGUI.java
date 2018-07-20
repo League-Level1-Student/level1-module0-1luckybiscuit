2bd6ae2a60eae38c8d17033f2fa8f8e543fc96ef
1luckybiscuit <league@WTS-IM-6.attlocal.net>
2018-07-20 14:59:32 -0700

    Level 1 Hacker Now!!!
    Currently on PhotoQuiz. I am a certified level 1 hacker and will hack
    ur mum if you give me permission

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -15,31 +15,31 @@ import javax.swing.JPanel;
 public class MyFirstSwingGUI {
 	public static void main(String[] args) {
 		//1. Create and initialize an object of the JFrame class
-
+		JFrame wut = new JFrame();
 		//2. Set your JFrame object to be visible
-
+		wut.setVisible(true);
 		//3. Run your program. Do you see your window? It's probably very small.
 		
 		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+		wut.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//5. Create and initialize an object of the JPanel class
-
+		JPanel why = new JPanel();
 		//6. Create and initialize an object of the JLabel class
-
+		JLabel where = new JLabel();
 		//7. Set the text of the JLabel to a lovely greeting.
-
+		where.setText("You look like a picture today!");
 		//8. Add the JPanel object to the JFrame
-
+		wut.add(why);
 		//9. Add the JLabel object to the JPanel
-
+		why.add(where);
 		//10. Pack your JFrame.
-
-		//11. Run your program again. Do you see your message.
+		wut.pack();
+		//11. Run your program again. Do you see your message? HECK YEAH I DO
 		
 		//12. Use the loadImage method to set the icon of the JLabel object.  
-
+		where.setIcon(loadImage());
 		//13. Re-pack the JFrame object.
-		
+		wut.pack();
 		//14. Run the program one more time. Do you see the image?
 	}
 	
--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,21 +22,24 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+                String cursed = "https://i.kinja-img.com/gawker-media/image/upload/s--qmP29ccq--/c_scale,f_auto,fl_progressive,q_80,w_800/inzuqhcz6vvxpdodiuph.png";
 		// 2. create a variable of type "Component" that will hold your image
-
+                Component image;
 		// 3. use the "createImage()" method below to initialize your Component
-
+                createImage(cursed);
 		// 4. add the image to the quiz window
-
+                quizWindow.add(image);
 		// 5. call the pack() method on the quiz window
-
+                quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+                int curseCounter = JOptionPane.showOptionDialog(null, "Is this what you see in your nightmares?", "Cursed Image", 0, Object["YES", "NO"],);
 		// 7. print "CORRECT" if the user gave the right answer
-
+                	if(curseCounter == 1) {
+                		JOptionPane.showMessageDialog(null, "CONGRATULATIONS. THIS IS A CURSED IMAGE");
+                	}else {
 		// 8. print "INCORRECT" if the answer is wrong
-
+                		JOptionPane.showMessageDialog(null, "NO? DARN...");
+                	}
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
 
 		// 10. find another image and create it (might take more than one line of code)
