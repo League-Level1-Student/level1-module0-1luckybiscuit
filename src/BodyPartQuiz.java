6a4e57279e4c2b6e4725499c073d1ead58cbb7a7
League <league@WTS9.attlocal.net>
2018-08-01 19:56:03 -0700

    BodyPartQuiz --> DuelingButtons
    Now THAT was EPIC
    ⁣😥    😫  😒😣😒
    😒😒  😒 😒    😲
    😩 😢 😲 ⁣😤    😠
    😒  😒😒 😞    😤
    😭    😖  😒😔😫

--- src/BodyPartQuiz.java
+++ src/BodyPartQuiz.java
@@ -19,10 +19,10 @@ public class BodyPartQuiz {
 	// or if you prefer, get celebrity photos from the Internet, place them in the default
 	// package, and change the names below.
 	
-	String firstImage = "src/arnold.jpeg";
-	String secondImage = "src/leonardo.jpeg";
-	String thirdImage = "src/morgan.jpeg";
-	String fourthImage = "src/jack.jpeg";
+	String firstImage = "src/bay.jpg";
+	String secondImage = "src/danny.jpg";
+	String thirdImage = "src/mike.jpg";
+	String fourthImage = "src/pitbull.jpg";
 	
 	JFrame window = new JFrame();
 	JPanel panel = new JPanel();
@@ -31,26 +31,57 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+		int score = 0;
 		// 2. Set the size of the window in the initializeGui() method below
-
-		for (int i = 0; i < 4; i++) {
-			
+		for (int i = 1; i < 5; i++) {
+			if(i == 1) {
 			// 4. Ask the user who this person is and store their answer
-			String guess= JOptionPane.showInputDialog("who is this?");
+			String guess= JOptionPane.showInputDialog("Who is this?");
 			
 			// 5. Check their answer. If they guessed correctly:
 			// -- Tell them they are right and increase the score by 1
-
+			if(guess.equalsIgnoreCase("Michael Bay")) {
+				score += 1;
+				JOptionPane.showMessageDialog(null,"Correct!");
+			}
 			// 6. Otherwise:
 			// -- Tell them they are wrong and who the person is
-
+			else {
+				JOptionPane.showMessageDialog(null,"Wow, you don't know who that is? That's Michael Bay!");
+			}
 			// 7. Use the showNextImage() method below to get the next image
-			showNextImage();
 		    // 8. Show them their current score
-			
 			// 9. .... repeat for all your images.....
-
+			}
+			if(i == 2) {
+				String guess= JOptionPane.showInputDialog("Who is this?");
+				if(guess.equalsIgnoreCase("Danny Devito")) {
+					score += 1;
+					JOptionPane.showMessageDialog(null,"Correct!");
+				} else {
+					JOptionPane.showMessageDialog(null,"Wow, you don't know who that is? That's Danny Devito!");
+				}
+				}
+			if(i == 3) {
+				String guess= JOptionPane.showInputDialog("Who is this?");
+				if(guess.equalsIgnoreCase("Mike Wazowski")) {
+					score += 1;
+					JOptionPane.showMessageDialog(null,"Correct!");
+				} else {
+					JOptionPane.showMessageDialog(null,"Wow, you don't know who that is? That's Mike Wazowski!");
+				}
+				}
+			if(i == 4) {
+				String guess= JOptionPane.showInputDialog("Who is this?");
+				if(guess.equalsIgnoreCase("Pitbull")) {
+					score += 1;
+					JOptionPane.showMessageDialog(null,"Correct!");
+				} else {
+					JOptionPane.showMessageDialog(null,"Wow, you don't know who that is? That's Pitbull!");
+				}
+				}
+			JOptionPane.showMessageDialog(null,"Your score is " + score);
+			showNextImage();
 
 		}
 
@@ -75,7 +106,7 @@ public class BodyPartQuiz {
 		window.add(panel);
 		
 		// 3. Change the size of the window so that you can only see part of the image.		
-		window.setSize(500,500);
+		window.setSize(300,300);
 		
 		showNextImage();
 		
